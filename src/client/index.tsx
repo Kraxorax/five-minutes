d6f9fea1a59efe60509c6e23c225fef97c50f173
Kraxorax <gmarkokralj@gmail.com>
2021-11-06 18:09:44 +0400

    socket proxy fail

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import App from './Components/app';
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:8050')
+const socket = io()
 socket.emit('msg', '123test')
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -80,13 +80,18 @@ module.exports = {
         target: 'http://localhost:8050',
         secure: false,
         changeOrigin: true
+      },
+      '/socket.io': {
+        target: 'http://localhost:8050/socket.io',
+        ws: true
       }
-    },
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
     }
+
+    // headers: {
+    //   "Access-Control-Allow-Origin": "*",
+    //   "Access-Control-Allow-Methods": "*",
+    //   "Access-Control-Allow-Headers": "*"
+    // }
   },
   plugins: [
     new CleanWebpackPlugin([outputDirectory]),
